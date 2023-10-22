7c0746a1367ec7d497aab65d9742d5ef2baa4b1c
abdurrehman0206 <abdurrehnmansaleem5@gmail.com>
2023-10-22 13:54:47 +0500

    Added hero

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,15 @@
+import LandingHero from "./components/LandingHero/LandingHero";
+import ProductCard from "./components/ProductCard/ProductCard";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App-container">
+      <div className="App">
+        <LandingHero />
+        <ProductCard />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/components/LandingHero/LandingHero.jsx
+++ frontend/src/components/LandingHero/LandingHero.jsx
@@ -3,7 +3,13 @@ import React from "react";
 function LandingHero() {
   return (
     <div className="landing-hero-container">
-      <div className="landing-hero">Landing Hero</div>
+      <div className="landing-hero">
+        <div className="landing-hero-content">
+          <h1>Where Sweet Dreams Come to Life.</h1>
+          <h3>Unlock a World of Sweet Surprises - One Candy at a Time.</h3>
+          <button className="btn-primary">Discover Sweet Delights</button>
+        </div>
+      </div>
     </div>
   );
 }
