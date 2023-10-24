bf1136e01761197a98c33386e969bd342aedfbff
abdurrehman0206 <abdurrehnmansaleem5@gmail.com>
2023-10-24 17:29:18 +0500

    Blogs Template

---
+++ frontend/src/components/BlogCard/BlogCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BlogCard() {
+  return (
+    <div className="blog-card-container">
+      <div className="blog-card"></div>
+    </div>
+  );
+}
+
+export default BlogCard;
---
+++ frontend/src/pages/Blogs/Blogs.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Blogs() {
+  return (
+    <div className="blogs-wrapper">
+      <div className="blogs"></div>
+    </div>
+  );
+}
+
+export default Blogs;
