c1b06fbcbcd57781d323f632a823d31e88f3e01b
abdurrehman0206 <abdurrehnmansaleem5@gmail.com>
2023-10-24 21:02:52 +0500

    Added Blog Card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import LandingHero from "./components/LandingHero/LandingHero";
 import ProductCard from "./components/ProductCard/ProductCard";
 import AboutUs from "./pages/AboutUs/AboutUs";
-
+import BlogCard from "./components/BlogCard/BlogCard";
 function App() {
   return (
     <div className="App-container">
       <div className="App">
         <LandingHero />
         <ProductCard />
-        <AboutUs/>
+        <AboutUs />
+        <BlogCard />
       </div>
     </div>
   );
--- frontend/src/components/BlogCard/BlogCard.jsx
+++ frontend/src/components/BlogCard/BlogCard.jsx
@@ -1,9 +1,21 @@
 import React from "react";
-
+import { SlCalender } from "react-icons/sl";
 function BlogCard() {
   return (
     <div className="blog-card-container">
-      <div className="blog-card"></div>
+      <div className="blog-card">
+        <img
+          src="https://thumbs.dreamstime.com/z/valentines-day-candy-hearts-4014974.jpg?w=992"
+          alt="Blog Post"
+        />
+        <div className="blog-card-info">
+          <small>
+            <SlCalender />
+            September 23 , 2020
+          </small>
+          <h1>Top 10 Candies of the Month</h1>
+        </div>
+      </div>
     </div>
   );
 }
