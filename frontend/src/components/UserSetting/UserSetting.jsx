be21ae8baf262f593badb0b918c8019b5c59311f
Muhammad Tayyab <muhammadtayyab742002@gmail.com>
2023-10-29 21:27:45 +0500

    user settings

--- frontend/src/components/UserSetting/UserSetting.jsx
+++ frontend/src/components/UserSetting/UserSetting.jsx
@@ -1,26 +1,22 @@
 import React, { useState } from "react";
 import profile from "../../assets/profile.jpg";
 import { TfiUpload } from "react-icons/tfi";
+import { FiEdit } from "react-icons/fi";
+import { MdOutlineCancel } from "react-icons/md";
+
 import Input from "../Common/Input";
 function UserSetting() {
   const [userDetails, setUserDetails] = useState({
-    firstName: {
-      value: "jhon",
-      edit: true,
-    },
-    lastName: {
-      value: "",
-      edit: false,
-    },
-    email: {
-      value: "",
-      edit: false,
-    },
+    firstName: "jhon",
+    lastName: "Philip",
+    email: "abc@gmail.com",
   });
+  const [edit, setEdit] = useState(false);
+  const [changePassword, setChangePassword] = useState(false);
   const changeHandle = (e) => {
     setUserDetails({
       ...userDetails,
-      [e.target.name]: { ...e.target.name, value: e.target.value },
+      [e.target.name]: e.target.value,
     });
   };
   return (
@@ -32,27 +28,63 @@ function UserSetting() {
             <li>
               <span>First Name</span>
               <Input
-                className="user-detail"
+                className={!edit ? "user-detail-inp" : ""}
                 type="text"
-                value={userDetails.firstName.value}
+                value={userDetails.firstName}
                 onChange={changeHandle}
                 name="firstName"
-                readOnly={!userDetails.firstName.edit}
+                readOnly={!edit}
               />
             </li>
             <li>
               <span>Last Name </span>
-              <p>Philip</p>
+              <Input
+                className={!edit ? "user-detail-inp" : ""}
+                type="text"
+                value={userDetails.lastName}
+                onChange={changeHandle}
+                name="lastName"
+                readOnly={!edit}
+              />
             </li>
             <li>
               <span>Email </span>
-              <p>abc@xyz.com</p>
+              <Input
+                className={!edit ? "user-detail-inp" : ""}
+                type="text"
+                value={userDetails.email}
+                onChange={changeHandle}
+                name="email"
+                readOnly={!edit}
+              />
             </li>
-            <li>
-              <button className="btn-box-outline change-password-btn">
+            {edit && (
+              <li>
+                <button
+                  className="btn-box-primary"
+                  onClick={() => setEdit(false)}
+                >
+                  Save
+                </button>
+              </li>
+            )}
+            <li className="change-upload-btn-container">
+              <button
+                className="btn-box-outline change-password-btn"
+                onClick={() => setChangePassword(!changePassword)}
+              >
                 Change password
               </button>
+              <button className="btn-box-primary upload-img-btn">
+                Upload Image
+              </button>
             </li>
+            <button
+              className="user-detail-edit-btn"
+              onClick={() => setEdit(!edit)}
+            >
+              {edit ? <MdOutlineCancel /> : <FiEdit />}
+            </button>
           </ul>
         </div>
         <div className="user-setting-profile-pic-section">
@@ -67,9 +99,40 @@ function UserSetting() {
               <TfiUpload />
             </button>
           </div>
-
-          {/* <button className="btn-box-outline">Upload New</button> */}
         </div>
+        {/* password Change Form */}
+        {changePassword && (
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+            className="pass-change-form-wrapper"
+          >
+            <div className="pass-change-form">
+              <h3>Change Password</h3>
+              <div className="pass-change-form-inputs">
+                <Input
+                  label="Old Password"
+                  type="password"
+                  name="oldPassword"
+                />
+                <Input
+                  label="New Password"
+                  type="password"
+                  name="newPassword"
+                />
+                <Input
+                  label="Confirm Password"
+                  type="password"
+                  name="confirmPassword"
+                />
+              </div>
+            </div>
+            <div>
+              <button className="btn-box-primary">Submit</button>
+            </div>
+          </form>
+        )}
       </section>
     </div>
   );
