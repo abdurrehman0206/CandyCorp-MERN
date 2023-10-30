9eb80c3df8896af527c5847959740c81bc35e4e6
abdurrehman0206 <abdurrehnmansaleem5@gmail.com>
2023-10-30 09:00:09 +0500

    Orders backend connection

--- frontend/src/components/Orders/Orders.jsx
+++ frontend/src/components/Orders/Orders.jsx
@@ -1,43 +1,74 @@
-import React from "react";
+import React, { useState, useLayoutEffect } from "react";
+import { useAuthContext } from "../../hooks/useAuthContext";
 function Orders() {
-  const orders = [
-    {
-      id: 1,
-      date: "2020-01-01",
-      status: "in-progress",
-      total: 100,
-    },
-    {
-      id: 2,
-      date: "2020-01-01",
-      status: "cancelled",
-      total: 100,
-    },
-    {
-      id: 3,
-      date: "2020-01-01",
-      status: "shipped",
-      total: 100,
-    },
-    {
-      id: 4,
-      date: "2020-01-01",
-      status: "delivered",
-      total: 100,
-    },
-  ];
+  const { user } = useAuthContext();
+  const [orders, setOrders] = useState([]);
+  useLayoutEffect(() => {
+    if (!user) {
+      return;
+    }
+    const getOrders = async () => {
+      const user = JSON.parse(localStorage.getItem("user"));
+      if (user) {
+        const response = await fetch(
+          `${process.env.REACT_APP_BASE_URL}/api/orders/${user.id}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${user.token}`,
+            },
+          }
+        );
+        const json = await response.json();
+        if (json.success) {
+          setOrders(json.data);
+        } else {
+          console.log(json.error);
+        }
+      }
+    };
+    getOrders();
+  }, [user]);
+  // const orders = [
+  //   {
+  //     id: 1,
+  //     date: "2020-01-01",
+  //     status: "in-progress",
+  //     total: 100,
+  //   },
+  //   {
+  //     id: 2,
+  //     date: "2020-01-01",
+  //     status: "cancelled",
+  //     total: 100,
+  //   },
+  //   {
+  //     id: 3,
+  //     date: "2020-01-01",
+  //     status: "shipped",
+  //     total: 100,
+  //   },
+  //   {
+  //     id: 4,
+  //     date: "2020-01-01",
+  //     status: "delivered",
+  //     total: 100,
+  //   },
+  // ];
+
+  //TODO: Add no orders render
   return (
     <div className="Orders-wrapper">
       <div className="Orders">
         {orders.map((order) => (
           <div key={order.id} className="Order-content">
-            <h3>Order # {order.id}</h3>
+            <h4>{order._id}</h4>
             <div className="order-status">
               <span className={`order-status-pil ${order.status}`}>
                 {order.status}
               </span>
 
-              <p>{order.date}</p>
+              <p>{new Date(order.createdAt).toLocaleString()}</p>
             </div>
             <div className="view-order">
               <button className="view-order-btn">View Order</button>
