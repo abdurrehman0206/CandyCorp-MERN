8651c0c4b883e3c4bc866a8aeda14aaf72787359
Muhammad Tayyab <muhammadtayyab742002@gmail.com>
2023-10-30 18:58:04 +0500

    Contact Page

--- frontend/src/pages/ContactUs/ContactUs.jsx
+++ frontend/src/pages/ContactUs/ContactUs.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import Input from "../../components/Common/Input";
+import { CgFacebook } from "react-icons/cg";
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
+import Logo from "../../assets/Logo.png";
+const ContactUs = () => {
+  return (
+    <div className="contact-wrapper">
+      <div className="contact-container">
+        <div className="contact-img-section">
+          <img src={Logo} alt="Company Logo" />
+        </div>
 
-function ContactUs() {
-  return <div>ContactUs</div>;
-}
+        <div className="contact-form-container">
+          <h2>CONTACT US</h2>
+          <form onSubmit={(e) => e.preventDefault()}>
+            <Input type="text" placeholder="Name" />
+            <Input type="email" placeholder="Email" />
+            <textarea
+              
+              placeholder="Message..."
+              className="contact-message-area"
+            />
+            <button className="btn-box-primary">Submit →</button>
+          </form>
+
+          <div className="social-icons">
+            <i>
+              <CgFacebook />
+            </i>
+            <i>
+              <AiOutlineTwitter />
+            </i>
+            <i>
+              <AiFillInstagram />
+            </i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ContactUs;
