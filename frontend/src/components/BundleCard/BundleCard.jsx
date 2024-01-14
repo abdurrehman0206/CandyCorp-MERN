9cbd18d3bcc4a538b0e69d4fd4295b7ca1d83de4
abdurrehman0206 <abdurrehnmansaleem5@gmail.com>
2024-01-14 13:28:51 +0500

    Added Bundle Files

---
+++ frontend/src/components/BundleCard/BundleCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BundleCard(props) {
+  return (
+    <div className="bundle-card-container">
+      <div className="bundle-card"></div>
+    </div>
+  );
+}
+
+export default BundleCard;
---
+++ frontend/src/pages/Bundle/Bundle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Bundle() {
+  return (
+    <div className="bundle-wrapper">
+      <div className="bundle"></div>
+    </div>
+  );
+}
+
+export default Bundle;
